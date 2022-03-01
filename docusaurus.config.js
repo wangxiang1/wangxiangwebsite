d420104a6b8bbe87887118bb277419d811067d41
王响 <wangxiang1@bonc.com.cn>
2022-03-01 15:57:19 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,8 +13,8 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'wangxiang1', // Usually your GitHub org/user name.
+  projectName: 'wangxiangwebsite', // Usually your repo name.
 
   i18n: {
     defaultLocale: 'zh-CN',
